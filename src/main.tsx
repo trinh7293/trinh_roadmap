4c9e00b5dfcf43dd998afa8d7a039e7be4578ce9
trinh7293 <trinh7293@gmail.com>
2024-11-01 16:42:25 +0700

    WIP add_router

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,25 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
+// import App from './App'
+import Board from './routes/Board'
 
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-// ReactDOM.createRoot(document.getElementById('root')!).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <div>Hello world!</div>
+  },
+  {
+    path: '/boards',
+    element: <div>Hello world!</div>
+  },
+  {
+    path: '/board/:boardId',
+    element: <Board />
+  }
+])
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')!).render(<RouterProvider router={router} />)
---
+++ src/routes/BoardList.tsx
@@ -0,0 +1,11 @@
+import { AppState } from '../types'
+
+const selector = (state: AppState) => ({
+  user: state.user
+})
+
+function BoardList() {
+  return <div>ahihi</div>
+}
+
+export default BoardList
